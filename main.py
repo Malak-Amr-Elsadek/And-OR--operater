e1e539f0a2af0c19d0c59a0729e221d1331de02a
Malak-Amr-Elsadek <malakelsadek.2015@gmail.com>
2024-11-10 15:27:14 +0300

    And-Or      operaters

---
+++ main.py
@@ -0,0 +1,30 @@
+a=10
+b=15
+c=0
+
+if a and b and c:
+    print("All the numbers have boolean as true.")
+else:
+    print("At least one number is false.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+a=10
+b=10
+c=0    
+
+if a>0 or b>0:
+    print("Any one number is greter than 0.")
+else:
+    print("No number is greater by 0.")
+
